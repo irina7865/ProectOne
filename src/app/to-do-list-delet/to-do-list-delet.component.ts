5fcf6e4224938caa6e59dd97d0de5fb7027f5824
Ira <you@example.comzaranskairina@gmail.com>
2024-05-26 16:23:21 +0300

    TodolistStyle

--- src/app/to-do-list-delet/to-do-list-delet.component.ts
+++ src/app/to-do-list-delet/to-do-list-delet.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { ListAdd } from '../Massiv/item' ;
-import { NgFor } from '@angular/common'
+import { NgFor,NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-to-do-list-delet',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgClass],
   templateUrl: './to-do-list-delet.component.html',
   styleUrl: './to-do-list-delet.component.css'
 })
 export class ToDoListDeletComponent {
   array = ListAdd
-  moveTwo(x:number){
-  this.array = this.array.filter(itemId => itemId.id !== x)
-  
-  }
+  moveTwo(id: number): void
+   { this.array.forEach((item, index) => 
+    { if (item.id === id) { 
+      this.array.splice(index, 1); }})
+    ; }
+    
 }
